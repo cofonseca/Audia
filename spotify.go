4223b9688ccd65b3cd7618cf4c93e303a3be5b43
Corey Fonseca <cfonseca@blueport.com>
2020-11-27 12:35:26 -0500

    Cleaning up comments

--- spotify.go
+++ spotify.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-type spotifyApiCredentials struct {
+type spotifyAPICredentials struct {
 	ClientID  string `json:"clientID"`
 	ClientSec string `json:"clientSec"`
 }
@@ -21,18 +21,10 @@ type spotifyResponse struct {
 	Expiration  int    `json:"expires_in"`
 }
 
-// https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
-// POST to https://accounts.spotify.com/api/
-// Encode the body in application/x-www-form-urlencoded
-// Body should include grant_type: client_credentials
-// Header should include Authorization: Basic <base64 encoded client_id:client_secret>
-// client id: 1d2f80b18a3f494296d5c5d751eeb284
-// client secret: 4ddd02f31d96471d86c1905e34850b4a
-
 func connectToSpotify() string {
 	// Get Spotify API Credentials from Config
 	reqURL := "https://accounts.spotify.com/api/token"
-	var creds spotifyApiCredentials
+	var creds spotifyAPICredentials
 	configFile, err := ioutil.ReadFile("./config.json")
 	if err != nil {
 		fmt.Println("Error reading from config.json:", err)
